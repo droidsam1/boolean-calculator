bfaeb5b2ce751683e695b474f03b948907b7384b
droidsam1 <droidsam@gmail.com>
2022-10-24 20:52:02 +0200

    add not operator evaluation

--- src/main/java/com/droidsam/app/BooleanCalculator.java
+++ src/main/java/com/droidsam/app/BooleanCalculator.java
@@ -4,6 +4,11 @@ public class BooleanCalculator {
 
 
     public static boolean evaluate(String input) {
-        return Boolean.parseBoolean(input);
+        return Boolean.parseBoolean(
+                input.toLowerCase()
+                        .replace(" ", "")
+                        .replace("not", "!")
+                        .replace("!true", "false")
+                        .replace("!false", "true"));
     }
 }
--- src/test/java/com/droidsam/app/BooleanCalculatorTest.java
+++ src/test/java/com/droidsam/app/BooleanCalculatorTest.java
@@ -10,12 +10,22 @@ import java.util.stream.Stream;
 public class BooleanCalculatorTest {
 
     private static Stream<Arguments> shouldAcceptSingleValuesAsInput() {
-        return Stream.of(Arguments.of("TRUE", true), Arguments.of("FALSe", false));
+        return Stream.of(Arguments.of("TRUE", true), Arguments.of("FALSE", false));
+    }
+
+    private static Stream<Arguments> shouldAcceptTheNotOperator() {
+        return Stream.of(Arguments.of("NOT TRUE", false), Arguments.of("NOT FALSE", true));
     }
 
     @ParameterizedTest
     @MethodSource()
     public void shouldAcceptSingleValuesAsInput(String input, boolean expected) {
+        Assertions.assertEquals(expected, BooleanCalculator.evaluate(input));
+    }
+
+    @ParameterizedTest
+    @MethodSource()
+    public void shouldAcceptTheNotOperator(String input, boolean expected) {
         Assertions.assertEquals(expected, BooleanCalculator.evaluate(input));
     }
 }
